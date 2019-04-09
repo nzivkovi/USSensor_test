016476313d446bffe47bcdf984faeb0ce81fe14e
Nikolaz94 <nikola.z1994@gmail.com>
2019-04-10 00:41:27 +0200

    initial commit

---
+++ US_Sensor.cpp
@@ -0,0 +1,96 @@
+#include "US_Sensor.h"
+
+Sensor::Sensor(int trigPin, int echoPin)
+{
+  this->trigPin = trigPin;
+  this->echoPin = echoPin;
+  // Define echoPin as Input
+  pinMode(echoPin, INPUT);
+  // Define trigPin as Output
+  pinMode(trigPin, OUTPUT);
+}
+
+int Sensor::get_distance()
+{
+  //max time in microseconds to wait for the pulse to be completed
+  unsigned long max_delay_time = 3650;
+  int duration;
+  //clears the trigPin
+  //digitalWrite(this->trigPin, LOW);
+  //delayMicroseconds(2);
+  digitalWrite(this->trigPin, HIGH);
+  delayMicroseconds(2);
+  digitalWrite(this->trigPin, LOW);
+  duration = pulseIn(this->echoPin, HIGH, max_delay_time);
+  if (/*duration >= max_delay_time ||*/ duration == 0){
+    return 100;  
+  } else {
+    return int((duration / 2) / 29.0);    
+  }
+}
+
+/*int Sensor::get_distance()
+{
+  int arr[NB_SAMPLE];
+  //clears the trigPin
+  digitalWrite(this->trigPin, LOW);
+  delayMicroseconds(2);
+  int duration;
+  for(int i=0; i<25; i++)
+  {
+    digitalWrite(this->trigPin, HIGH);
+    delayMicroseconds (10);
+    digitalWrite(this->trigPin, LOW);
+    duration = pulseIn(this->echoPin, HIGH);
+    arr[i] = int(duration * 0.5 * 0.0344);
+  }
+  return find_median(arr);
+}
+
+int Sensor::find_median(int* measurements)
+{
+  return this->select(measurements, 0, 24, 12);
+}
+
+int Sensor::partition(int *arr, int left, int right)
+{
+  int x = arr[left];
+  int i = left;
+  int key;
+  for (int j = left + 1; j <= right; j++)
+  {
+    if (arr[j] <= x)
+    { 
+      i = i+1;
+      key = arr[i];
+      arr[i] = arr[j];
+      arr[j] = key;     
+    }
+  }
+  key = arr[i];
+  arr[i]= arr[left];
+  arr[left] = key;
+  return i;
+}
+
+int Sensor::select(int *arr, int left, int right, int i)
+{
+  if (left == right)
+  {
+    return arr[left];
+  }
+  int r = this->partition(arr, left, right);
+  int k = r - left + 1;
+  if (i == k)
+  {
+    return arr[r];
+  }
+  if (i < k)
+  {
+    return this->select(arr, left, r - 1, i);
+  }
+  else
+   {
+    return select(arr, r + 1, right, i - k);
+  }
+}*/
---
+++ US_sensor_test.ino
@@ -0,0 +1,49 @@
+#include "US_Sensor.h"
+
+
+//defines pins numbers
+#define TRIG_PIN_0 22
+#define ECHO_PIN_0 23
+#define TRIG_PIN_1 24
+#define ECHO_PIN_1 25
+#define TRIG_PIN_2 26
+#define ECHO_PIN_2 27
+#define TRIG_PIN_3 28
+#define ECHO_PIN_3 29
+#define TRIG_PIN_4 30
+#define ECHO_PIN_4 31
+
+/*granica koliko želim ispisa u loopu, koristim ju u if petlji*/
+#define MAX_VALUES 51
+
+/*pomoćna varijabla da si ograničim broj ispisa u loopu*/
+int tmp = 1; 
+
+/* Instances */
+Sensor* sensor_0 = new Sensor(TRIG_PIN_0, ECHO_PIN_0);
+Sensor* sensor_1 = new Sensor(TRIG_PIN_1, ECHO_PIN_1);
+Sensor* sensor_2 = new Sensor(TRIG_PIN_2, ECHO_PIN_2);
+Sensor* sensor_3 = new Sensor(TRIG_PIN_3, ECHO_PIN_3);
+Sensor* sensor_4 = new Sensor(TRIG_PIN_4, ECHO_PIN_4);
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  /* Check for obstacles */
+  if(tmp < MAX_VALUES || true){ 
+    Serial.print(sensor_0->get_distance());
+    Serial.print(" ");
+    Serial.print(sensor_1->get_distance());
+    Serial.print(" ");
+    Serial.print(sensor_2->get_distance());
+    Serial.print(" ");
+    Serial.print(sensor_3->get_distance());
+    Serial.print(" ");
+    Serial.print(sensor_4->get_distance());
+    Serial.println();
+  }
+  tmp += 1;
+  /* Avoid reading before the new distance is sampled */
+  delay(100);
+}
